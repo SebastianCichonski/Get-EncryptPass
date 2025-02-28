a0a0466c55c0c6848aa50d6c7b8fa43f40948101
SebastianCichonski <110123922+SebastianCichonski@users.noreply.github.com>
2025-02-28 07:53:56 +0100

    Update and rename Get-SREncryptPass.ps1 to Get-EncryptPass.ps1

--- Get-EncryptPass.ps1
+++ Get-EncryptPass.ps1
@@ -1,18 +1,18 @@
 <#
 .SYNOPSIS
-    Skrypt do zaszyfrowywania hase³ u¿ywanych w skryptach.
+    Skrypt do zaszyfrowywania haseÅ‚ uÅ¼ywanych w skryptach.
 
 .DESCRIPTION
-    Skrypt pobiera has³o w formie jawnej i zwraca zaszyfrowany ci¹ znaków. Do szyfrowania u¿ywane jest Windows Data Protection API. 
-    Oznacza to, ¿e poœwiadczenia zaszyfrowane w ten sposób s¹ mo¿liwe do odczytania tylko przez dany komputer oraz u¿ytkownika, który wykona³ to polecenie. 
-    Niestety skryptu z tak zaszyfrowanym has³em nie u¿yjemy na innych komputerach. Aby to umo¿liwiæ musimy dodatkowo wygenerowaæ klucz u¿ywaj¹c parametru -WithKey
-    w takim przypadku skrypt wygeneruje plik z has³em (C:\Temp\Secure\Password.txt) i plik z kluczem (C:\Temp\Secure\Secure.key), które mo¿na wyko¿ystaæ w cmdlecie ConvertFrom-SecureString. 
+    Skrypt pobiera hasÅ‚o w formie jawnej i zwraca zaszyfrowany ciÄ… znakÃ³w. Do szyfrowania uÅ¼ywane jest Windows Data Protection API. 
+    Oznacza to, Å¼e poÅ›wiadczenia zaszyfrowane w ten sposÃ³b sÄ… moÅ¼liwe do odczytania tylko przez dany komputer oraz uÅ¼ytkownika, ktÃ³ry wykonaÅ‚ to polecenie. 
+    Niestety skryptu z tak zaszyfrowanym hasÅ‚em nie uÅ¼yjemy na innych komputerach. Aby to umoÅ¼liwiÄ‡ musimy dodatkowo wygenerowaÃ¦ klucz uÅ¼ywajÄ…c parametru -WithKey
+    w takim przypadku skrypt wygeneruje plik z hasÅ‚em (C:\Temp\Secure\Password.txt) i plik z kluczem (C:\Temp\Secure\Secure.key), ktÃ³re moÅ¼na wykoÅ¼ystaÄ‡ w cmdlecie ConvertFrom-SecureString. 
 
 .PARAMETER Phrase
-    Has³o do zaszyfrowania. Parametr wymagany.
+    HasÅ‚o do zaszyfrowania. Parametr wymagany.
 
 .PARAMETER WithKey
-    Prze³¹cznik, który spowoduje wygenerowanie plików z has³em i kluczem.
+    PrzeÅ‚Ä…cznik, ktÃ³ry spowoduje wygenerowanie plikÃ³w z hasÅ‚em i kluczem.
 
 .INPUTS
     None
@@ -25,10 +25,10 @@
     Version:        1.1
     Author:         Sebastian Cichonski
     Creation Date:  1.2024
-    Projecturi:     https://gitlab.com/powershell1990849/get-srencryptpass
+    Projecturi:     https://
   
 .EXAMPLE
-    Get-SREncryptPass.ps1 -Phrase tajnehasl0
+    Get-EncryptPass.ps1 -Phrase tajnehasl0
 
     Encrypted Password: 
     01000000d08c9ddf0115d1118c7a00c04fc297eb01000000625d34456177704697ebf56209759b940000000002000000000003660000c0000000100000003e27cbf9439f69e23e
@@ -36,7 +36,7 @@
     d70a48082e92cd0d4ce4bfa4fc708750c4e51358        
 
 .EXAMPLE
-   Get-SREncryptPass.ps1 -Phrase tajnehasl0 -WithKey
+   Get-EncryptPass.ps1 -Phrase tajnehasl0 -WithKey
 
 #>
 
